8d6d14b586cdf697f6df40edc19989220d7fc378
Christian Bates <christianbates02@gmail.com>
2025-02-22 13:25:59 +0000

    added functionality to get vendors.

--- docs/notes.md
+++ docs/notes.md
@@ -3,4 +3,5 @@
 - Look into arrango db and other graph databases to store network topology data
 - Do we need another database such as prometheus for real time network polls?
 - Do we need a detection engine?
+- macvendors.com allows up to 1000 requests per day and 2 requests per second for free tier
 
--- src/networkkit/discovery_engine.py
+++ src/networkkit/discovery_engine.py
@@ -1,17 +1,23 @@
 from util import get_logger
 from scapy.all import ARP, Ether, srp
+from pprint import pprint
 import subprocess
 import re
 import time
+import requests
 
 logger = get_logger(__name__)
+
+OUI_URL = 'https://standards-oui.ieee.org/'
 
 class DiscoveryEngine:
     def __init__(self, networks):
         self.networks = networks
         self.sleep_interval = 15
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
+        self.oui_cache = self.fetch_oui_data()
         self.data = {}
-        
+    
     
     def discover_layer2_endpoints(self):
         try:
@@ -22,6 +28,49 @@ class DiscoveryEngine:
                 logger.info(f"Successfully discovered the following ip addresses on network {network}: {ips}")
         except subprocess.CalledProcessError as e:
             logger.error(f"Failed to execute 'nmap -sn' scan on {self.networks}. Exit code: {e.returncode}")
+    
+    
+    def set_mac_addresses(self):
+        try:
+            for ip in self.data:
+                output = subprocess.check_output(['arp', '-a', ip], text=True)
+                mac = self.parse_mac_address(output)
+                self.data[ip]['mac'] = mac
+                logger.info(f"Successfully discovered mac address {mac} for ip address {ip}.")
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Failed to execute 'arp -a' scan on {self.networks}. Exit code: {e.returncode}.")
+    
+    
+    def set_vendors(self):
+        for ip in self.data:
+            mac = self.data[ip]['mac']
+            if mac:
+                oui = self.parse_oui_from_mac(mac)
+                vendor = self.lookup_vendor(oui)
+                self.data[ip]['vendor'] = vendor
+                
+    
+    def parse_oui_from_mac(self, mac):
+        oui = '-'.join(mac.split(':')[0:3]).upper()
+        return oui
+    
+    
+    def lookup_vendor(self, oui):
+        if oui and oui in self.oui_cache:
+            vendor = re.search(f'{oui}.*', self.oui_cache).group().split('\t')[2].strip()
+            logger.info(f'Vendor found in oui cache: {vendor}.')
+            return vendor
+        logger.error(f'OUI not found in oui cache: {oui}.')
+        return None
+    
+    
+    def fetch_oui_data(self):
+        response = requests.get(OUI_URL, headers=self.headers)
+        if response.status_code == 200:
+            logger.info('Successfully fetched oui data.')
+            return response.text
+        logger.error('Failed to fetch oui data. Response code: ', response.status_code)
+        return None
     
     
     def parse_ip_addresses(self, output):
@@ -36,31 +85,12 @@ class DiscoveryEngine:
         mac = match.group() if match else None
         return mac
     
-    # curl https://api.macvendors.com/5c:26:0a:b3:8c:7a
-    def get_mac_addresses(self):
-        try:
-            for ip in self.data:
-                output = subprocess.check_output(['arp', '-a', ip], text=True)
-                mac = self.parse_mac_address(output)
-                self.data[ip]['mac'] = mac
-                logger.info(f"Successfully discovered mac address {mac} for ip address {ip}")
-        except subprocess.CalledProcessError as e:
-            logger.error(f"Failed to execute 'arp -a' scan on {self.networks}. Exit code: {e.returncode})")
-                
-        
-    def scan_ip_addresses(self):
-        pass
-    
     
     def run(self):
         logger.info('Starting discovery engine.')
         while True:
             self.discover_layer2_endpoints()
-            self.get_mac_addresses()
-            print(self.data)
+            self.set_mac_addresses()
+            self.set_vendors()
+            pprint(self.data)
             time.sleep(self.sleep_interval)
-
-
-if __name__ == '__main__':
-    discovery_engine = DiscoveryEngine('10.0.97.0/24')
-    discovery_engine.discover_layer2_endpoints()
